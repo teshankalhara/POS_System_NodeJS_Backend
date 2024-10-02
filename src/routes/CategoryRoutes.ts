6e6469cc508554b6533875e503bb10bc60133ad6
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-10-02 23:14:20 +0530

    catgegory_controller

---
+++ src/routes/CategoryRoutes.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+import { createCategory,getCategory } from '../controller/CategoryController'
+
+const router=express.Router()
+
+router.post('/create',createCategory)
+router.get('/get',getCategory)
